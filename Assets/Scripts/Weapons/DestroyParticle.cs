2ad195e39161c621b35b708bd29d4f649abeb01a
bennie12345 <bmuijldert@gmail.com>
2016-05-13 14:42:46 +0200

    refined particledestroying

---
+++ Assets/Scripts/Weapons/DestroyParticle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParticle : MonoBehaviour {
+
+    private ObjectPool _objectPoolScript;
+    private ParticleSystem _particleSystem;
+
+    private int _particleCount;
+    
+
+	void OnEnable () 
+    {
+        _particleSystem = transform.GetComponent<ParticleSystem>();
+        _objectPoolScript = GameObject.FindWithTag(Tags.OBJECTPOOL).GetComponent<ObjectPool>();
+        _particleCount = _particleSystem.particleCount;
+	}
+
+    void Update()
+    {
+        DisableParticle();
+    }
+
+    void DisableParticle()
+    {
+        if(_particleCount <= 0)
+        {
+            _objectPoolScript.PoolObject(this.gameObject);
+        }
+    }
+}
