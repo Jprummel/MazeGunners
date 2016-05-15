d20e312d1ddeb764d146e7d62f2b62fa45dc6342
Jprummel <jordiprummel@live.nl>
2016-05-15 17:38:59 +0200

    Sound Effect script

---
+++ Assets/Scripts/Audio/SoundEffects.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffects : MonoBehaviour {
+
+    public AudioClip[] soundEffects;
+    private AudioSource _audio;
+	// Use this for initialization
+	void Start () {
+        _audio = GetComponent<AudioSource>();
+	}
+	
+	public void PlaySound(int soundNumber)
+    {
+        _audio.PlayOneShot(soundEffects[soundNumber]);
+    }
+}
