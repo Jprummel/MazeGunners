99704799123e783137d909c2b43831532d010a59
Jprummel <jordiprummel@live.nl>
2016-05-17 11:31:03 +0200

    Rounds added

--- Assets/Scripts/Data/RoundData.cs
+++ Assets/Scripts/Data/RoundData.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class RoundData : MonoBehaviour {
 
     [SerializeField]private List<GameObject> _players;
     [SerializeField]private Text _RoundEndText;
+    private int _roundNumber;
 
 	// Use this for initialization
 	void Start () {
+        _roundNumber = 1;
         _players = new List<GameObject>();
 
         RoundStart();
@@ -25,8 +28,14 @@ public class RoundData : MonoBehaviour {
         _players.Remove(player);
         if (_players.Count == 1)
         {
-            RoundEnd();
-            StartCoroutine(GoToNextRound(1.5f));
+            if (_roundNumber <= 4)
+            {
+                StartCoroutine(GoToNextRound(1.5f));
+            }
+            if (_roundNumber > 4)
+            {
+                StartCoroutine(FinalRound(1.5f));
+            }
         }
     }
 
@@ -34,31 +43,44 @@ public class RoundData : MonoBehaviour {
     {
         _players.Clear();
 
-        if (GameObject.Find("Player 1") && GameObject.Find("Player 2") != null)
+        if (GameObject.Find(ObjectPoolNames.PLAYER1) && GameObject.Find(ObjectPoolNames.PLAYER2) != null)
         {
             _players.Add(GameObject.Find("Player 1"));
             _players.Add(GameObject.Find("Player 2"));
         }
         
-        if (GameObject.Find("Player 3") && GameObject.Find("Player 4") != null)
+        if (GameObject.Find(ObjectPoolNames.PLAYER3) && GameObject.Find(ObjectPoolNames.PLAYER4) != null)
         {
             _players.Add(GameObject.Find("Player 3"));
             _players.Add(GameObject.Find("Player 4"));
         }
     }
 
-    void RoundEnd()
-    {
-        _RoundEndText.text = _players[0].name + " Wins!";
-    }
+ 
 
     IEnumerator GoToNextRound(float waitTime)
     {
+        _roundNumber++;
         _RoundEndText.text = _players[0].name + " Wins!";        
         yield return new WaitForSeconds(waitTime);
         _RoundEndText.text = "Get ready for the next round";
         yield return new WaitForSeconds(waitTime);
         _RoundEndText.text = "";
         RoundStart();
+    }
+
+    IEnumerator FinalRound(float waitTime)
+    {
+        _RoundEndText.text = _players[0].name + "Wins the final round!"; 
+        yield return new WaitForSeconds(waitTime);
+        _RoundEndText.text = "Returning to menu in 3...";
+        yield return new WaitForSeconds(1);
+        _RoundEndText.text = "Returning to menu in 2...";
+        yield return new WaitForSeconds(1);
+        _RoundEndText.text = "Returning to menu in 1...";
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(0);
+
     }
 }
