dc06134daea01e42058c1c75486547cbd09f0b4a
bennie12345 <bmuijldert@gmail.com>
2016-05-17 11:02:25 +0200

    modified objectpool dataclass slightly

--- Assets/Scripts/Data/ObjectPoolNames.cs
+++ Assets/Scripts/Data/ObjectPoolNames.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class ObjectPoolNames : MonoBehaviour {
 
+    public const string PLAYER1 = "Player1";
+    public const string PLAYER2 = "Player2";
+    public const string PLAYER3 = "Player3";
+    public const string PLAYER4 = "Player4";
     public const string BULLET = "Bullet";
     public const string TRANSPARANTWALL = "TransparantWall";
     public const string MUZZLEFLASH = "MuzzleFlash";
--- Assets/Scripts/Data/Tags.cs
+++ Assets/Scripts/Data/Tags.cs
@@ -3,10 +3,6 @@ using System.Collections;
 
 public class Tags : MonoBehaviour {
 
-    public const string PLAYER1          = "Player1";
-	public const string PLAYER2          = "Player2";
-	public const string PLAYER3          = "Player3";
-	public const string PLAYER4          = "Player4";
     public const string PICKUP          = "PickUp";
     public const string OBJECTPOOL      = "ObjectPool";
     public const string WALL            = "Wall";
