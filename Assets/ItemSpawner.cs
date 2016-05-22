7b06bd0e7ca9849205bbf8641eee21ae8f7f4f0b
kaj loevesijn <sep_ul_tura@live.nl>
2016-05-22 12:27:27 +0200

    the kraken says hi

--- Assets/ItemSpawner.cs
+++ Assets/ItemSpawner.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ItemSpawner : MonoBehaviour {
-
+	[SerializeField]private List<GameObject> powerUps = new List<GameObject> ();
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		int randomInt = Random.Range (0, 100);
+		if (randomInt >= 90) {//spawn a powerup
+			float margin = 100 / powerUps.Count;
+			randomInt = Random.Range (0, 100);
+			for (int i = 0; i < powerUps.Count; i++) {
+				if (randomInt >= margin * powerUps.Count - 1 && randomInt <= margin * powerUps.Count) {
+					Debug.Log ("powerupChosen");
+				}
+			}
+
+		}
 	}
 }
